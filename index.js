0c51c98c586546c3ff629cd815255a2ca28588f4
JyotiGhising1 <83853614+JyotiGhising1@users.noreply.github.com>
2024-02-02 22:53:25 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockValue){
+    const hqValue = 42;
+    let distance;
+    if(blockValue >= hqValue){
+        distance = blockValue - hqValue;
+    }
+    else{
+        distance= hqValue-blockValue;
+    }
+    return distance;
+}
+function distanceFromHqInFeet(blockValue){
+    const blocks = distanceFromHqInBlocks(blockValue);
+    const distanceInfeet = blocks * 264;
+    return distanceInfeet;
+}
+function distanceTravelledInFeet(start, destination){
+    const distanceInBlock = Math.abs(destination - start);
+    const totalDistanceTravelled = 264 * distanceInBlock;
+    return totalDistanceTravelled;
+}
+function calculatesFarePrice(start, destination){
+    const ratePerFoot = 0.02;
+    const maxRate = 25;
+    const minDistance = 400;
+    const maxDistance = 2000;
+    const notAllowedZone = 2500;
+    const totalDistanceInFeet = distanceTravelledInFeet(start, destination);
+    if(totalDistanceInFeet <= minDistance){
+        return 0;
+    }
+    else if(totalDistanceInFeet<=maxDistance){
+        return (totalDistanceInFeet-minDistance)* ratePerFoot;
+    }
+    else if(totalDistanceInFeet<=notAllowedZone){
+        return maxRate;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+
+}
